d9733392d4e52831fe7f7881ddad483ceb845300
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-05-18 02:18:54 -0300

    finished testing simulator,succesfully

--- +VehicleDynamicsLateral/@Simulator/Simulator.m
+++ +VehicleDynamicsLateral/@Simulator/Simulator.m
@@ -1,37 +1,36 @@
-classdef Simulator
+classdef Simulator<handle
 	% Simulator Vehicle dynamics simulator
 	% The simulator receives a vehicle object that inherits from VehicleSimple, simulates its behavior during a given time span and provides its behavior during time via its properties. Each property is a (timespan, 1) vector in which each value represents that parameter's value in time.
 	methods
         % Constructor
-        function self = TireLinear(vehicle, tspan)
-                self.Vehicle = vehicle;
-                self.TSpan = tspan;
-            end
+        function self = Simulator(vehicle, tspan)
+            self.Vehicle = vehicle;
+            self.TSpan = tspan;
         end
 
-		function simulate = Simulate(self)
-
+		function Simulate(self)
 			% TODO: gravity can be passed to the simulator so vertical load and other forces are calculated here
 
 			% integration
 			% if vehicle is articulated, adds mass matrix as an integration option
-			if isa(self.Vehicle, VehicleDynamicsLateral.VehicleArticulated)
-				options = odeset('Mass', @System.MassMatrix);
-				[TOUT,XOUT] = ode45(@(t, estados) System.Model(t, estados), self.TSpan, self.Vehicle.getInitialState(), options);
+			if isa(self.Vehicle, 'VehicleDynamicsLateral.VehicleArticulated')
+				options = odeset('Mass', @self.Vehicle.MassMatrix);
+				[TOUT,XOUT] = ode45(@(t, estados) self.Vehicle.Model(t, estados), self.TSpan, self.Vehicle.getInitialState(), options);
 
 				% retrieve states exclusive to the articulated vehicle
-				self.dPHI = XOUT(:, 7);           % Articulation rate [rad/s]
-				self.PHI = XOUT(:, 8);            % Articulation angle [rad]
+				self.dPHI = XOUT(:, 7);
+				self.PHI = XOUT(:, 8);
 			else
-				[TOUT, XOUT] = ode45(@(t, estados) System.Model(t, estados), self.TSpan, self.Vehicle.getInitialState());
+				[TOUT, XOUT] = ode45(@(t, estados) self.Vehicle.Model(t, estados), self.TSpan, self.Vehicle.getInitialState());
+			end
 
 			% Retrieving states post integration
-			self.dPSI = XOUT(:, 1);           % Yaw rate [rad/s]
-			self.ALPHAT = XOUT(:, 2);         % Side slip angle [rad]
-			self.PSI = XOUT(:, 3);            % Yaw angle [rad]
-			self.XT = XOUT(:, 4);             % CG horizontal position [m]
-			self.YT = XOUT(:, 5);             % CG vertical position [m]
-			self.VEL = XOUT(:, 6);            % CG velocity [m/s]
+			self.dPSI = XOUT(:, 1);
+			self.ALPHAT = XOUT(:, 2);
+			self.PSI = XOUT(:, 3);
+			self.XT = XOUT(:, 4);
+			self.YT = XOUT(:, 5);
+			self.VEL = XOUT(:, 6);
         end
 	end
 
