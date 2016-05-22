3d74dcc6ef5433bb9b1746ad9e0d568ce46d3953
andresmendes <asmgma@gmail.com>
2016-05-22 17:53:40 -0300

    updating

--- ManualTests.m
+++ ManualTests.m
@@ -2,42 +2,20 @@ clear all                   % Clear workspace
 close all                   % Closing figures
 clc                         % Clear command window
 
+% Choosing tire
+TireModel = VehicleDynamicsLateral.TirePacejka();
+% Choosing vehicle
+System = VehicleDynamicsLateral.VehicleArticulatedLinear();
+System.tire = TireModel;
+% Choosing simulation
 T = 6;                      % Total simulation time [s]
 resol = 50;                 % Resolution
 TSPAN = 0:T/resol:T;        % Time span [s]
+simulator = VehicleDynamicsLateral.Simulator(System, TSPAN);
 
-%% Tire parameters
-% Chosen tire: <TirePacejka.html TirePacejka.m>.
-%
-
-% TireModel = VehicleDynamicsLateral.TireLinear;
-%
-% %% Vehicle parameters
-% % Chosen Vehicle: <VehicleSimpleNonlinear.html VehicleSimpleNonlinear.m>.
-%
-% System = VehicleDynamicsLateral.VehicleSimpleNonlinear();
-% System.tire = TireModel;
-% simulator = VehicleDynamicsLateral.Simulator(System, TSPAN);
-% simulator.Simulate();
-
-% % Retrieving states
-% XT = simulator.XT;
-% YT = simulator.YT;
-% PSI = simulator.PSI;
-% VEL = simulator.VEL;
-% ALPHAT = simulator.ALPHAT;
-% dPSI = simulator.dPSI;
-
-% g = VehicleDynamicsLateral.Graphics(simulator);
-% g.Frame(0);
-% g.Animation(0);
-
-truck = VehicleDynamicsLateral.VehicleArticulatedNonlinear;
-truck.tire = VehicleDynamicsLateral.TireLinear;
-%
-simulator = VehicleDynamicsLateral.Simulator(truck, TSPAN);
+% Simulation
 simulator.Simulate();
-%
+
 % Retrieving states
 XT = simulator.XT;
 YT = simulator.YT;
@@ -50,4 +28,4 @@ dPHI = simulator.dPHI;
 
 g = VehicleDynamicsLateral.Graphics(simulator);
 g.Frame(0);
-% % g.Animation(0);
+% g.Animation(0);
