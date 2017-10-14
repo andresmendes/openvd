b055dc74f67d8273b4059ca1f49d3c687022876d
andresmendes <asmgma@gmail.com>
2017-10-14 17:12:05 -0300

    Deleting openvd zip file after installation.

--- post_install.m
+++ post_install.m
@@ -1,7 +1,7 @@
 function post_install(var)
 
-% This function copies the documentation from the zip file to the octave package installation directory.
-% This function is necessary because the OpenVD GitHub Doc page is hosted at /docs and Octave pkg function installs the documentation only from /docs
+% This function copies the documentation (/docs directory) from the zip file to the octave package installation directory.
+% This function is necessary because the OpenVD GitHub Doc page is hosted at /docs and Octave pkg function installs the documentation only from /doc.
 
 currentdir = pwd;
 pkgdir = var.dir;
@@ -14,6 +14,7 @@ cd ..
 unzip('openvd.zip');
 movefile('openvd/docs','openvd-0.0.0/');
 rmdir('openvd','s');
+delete('openvd.zip')
 
 cd(currentdir)
 
